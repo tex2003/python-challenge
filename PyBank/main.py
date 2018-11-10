75facbc3c7568e552c16758e8f3d7381efc319f8
Tex2003 <pamela.stadler@gmail.com>
2018-11-09 22:28:34 -0600

    initial commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,53 @@
-print ("hello world")
+import csv 
+import os
 
+csvpath = os.path.join("Resources", "budget_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    print ("Financial Analysis")
+    print ("------------------------")
+    total = sum(1 for _ in csvfile)
+    print("Total Months: " + str(total))
+    totalchanges = total - 1
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    prof = 0
+    for row in csv.reader(csvfile):
+        prof += int(row[1])
+    print ("Total: $" + str(prof))
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    act = 0
+    for row in csv.reader(csvfile):
+        first = int(row[1])
+        for row1 in csv.reader(csvfile):
+             second = int(row1[1])
+    act += second - first   
+    ac = act/totalchanges
+    x = "%.2f" % ac  
+    print ("Average Change: $" + str(x))
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    rows = []
+    for i, row in enumerate(csvreader):
+        first = int(row[1])
+        for row in csv.reader(csvfile):
+            dater = str(row[0])
+            second = int(row[1])
+            #print(first)
+            #print(second)
+            act = first - second
+            mydict = {"date": dater, "difference": act} 
+            mylist= [dater, act] 
+            rows.append(mylist)
+            #print (mydict)
+        print ("Greatest Increase in Profits:") 
+        print ("Greatest Decrease in Profits:") 
+
+#Greatest Increase in Profits: Feb-2012 ($1926159)
+#Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,35 @@
-print ("hello world")
+import csv 
+import os
+
+csvpath = os.path.join("Resources", "election_data.csv")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    print ("Election Results")
+    print ("------------------------")
+    total = sum(1 for _ in csvfile)
+    print("Total Votes: " + str(total))
+    print ("------------------------")
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    
+
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+#As an example, your analysis should look similar to the one below:
+#Election Results
+#-------------------------
+#Total Votes: 3521001
+#-------------------------
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+#O'Tooley: 3.000% (105630)
+#-------------------------
+#Winner: Khan
+#-------------------------
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
